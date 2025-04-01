aa5c15bd160a317c9b1baa4398570262f9ca28fd
farhan <farhan.gh555@gmail.com>
2025-04-01 15:51:26 +0300

    my second commit <><>

--- src/main/java/com/s3rha/spring/SpringSecurityApplication.java
+++ src/main/java/com/s3rha/spring/SpringSecurityApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 public class SpringSecurityApplication {
 
     public static void main(String[] args) {
+        System.out.println("this is for a commit test ");
         SpringApplication.run(SpringSecurityApplication.class, args);
     }
 
