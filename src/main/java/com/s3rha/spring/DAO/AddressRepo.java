426aa8b677a642ae6119aea933d0f8f4d7302fad
farhan <farhan.gh555@gmail.com>
2025-09-05 00:17:11 +0300

    findByStoreAccount for address

--- src/main/java/com/s3rha/spring/DAO/AddressRepo.java
+++ src/main/java/com/s3rha/spring/DAO/AddressRepo.java
@@ -1,7 +1,14 @@
 package com.s3rha.spring.DAO;
 
+import com.s3rha.spring.entity.Account;
 import com.s3rha.spring.entity.Address;
+import com.s3rha.spring.entity.StoreAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface AddressRepo extends JpaRepository <Address,Long>{
+    List<Address> findByStoreAccount(StoreAccount storeAccount);
+
 }
