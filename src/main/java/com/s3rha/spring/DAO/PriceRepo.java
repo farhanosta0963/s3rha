6fc6db52b0e1a1a049192d68382187fdc78eaa4e
farhan <farhan.gh555@gmail.com>
2025-09-04 22:40:33 +0300

    findProductsByStoreAccount

--- src/main/java/com/s3rha/spring/DAO/PriceRepo.java
+++ src/main/java/com/s3rha/spring/DAO/PriceRepo.java
@@ -4,10 +4,16 @@ import com.s3rha.spring.entity.Price;
 import com.s3rha.spring.entity.Product;
 import com.s3rha.spring.entity.StoreAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface PriceRepo extends JpaRepository <Price,Long>{
     List<Price> findByProduct(Product product) ;
-    List<Price> findByStoreAccount(StoreAccount  storeAccount) ;
+//    List<Price> findByStoreAccount(StoreAccount  storeAccount) ;
+
+
+    @Query("SELECT DISTINCT p.product FROM Price p WHERE p.storeAccount = :storeAccount")
+    List<Product> findProductsByStoreAccount(@Param("storeAccount") StoreAccount storeAccount);
 }
